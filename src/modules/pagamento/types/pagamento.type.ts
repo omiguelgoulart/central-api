a447901f2e074606367d26e1e61c1998709479f2
Luis Miguel Goulart <luismiguelgoulart@gmail.com>
2026-04-02 10:57:06 -0300

    :recycle: refactor(back): update src/modules/pagamento/types/pagamento.type.ts

--- src/modules/pagamento/types/pagamento.type.ts
+++ src/modules/pagamento/types/pagamento.type.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
-import { assinaturaSchema, faturaSchema, pagamentoSchema } from "../schemas/pagamento.schema";
+import { assinaturaSchema, faturaSchema, pagamentoSchema, pagamentoIngressoSchema, updatePagamentoIngressoSchema } from "../schemas/pagamento.schema";
 
 export type CreatePagamentoInput = z.infer<typeof pagamentoSchema>;
 export type CreateFaturaInput = z.infer<typeof faturaSchema>;
 export type UpdateFaturaInput = Partial<CreateFaturaInput>;
 export type CreateAssinaturaInput = z.infer<typeof assinaturaSchema>;
 export type UpdateAssinaturaInput = Partial<CreateAssinaturaInput> & { fimEm?: string };
+export type CreatePagamentoIngressoInput = z.infer<typeof pagamentoIngressoSchema>;
+export type UpdatePagamentoIngressoInput = z.infer<typeof updatePagamentoIngressoSchema>;
